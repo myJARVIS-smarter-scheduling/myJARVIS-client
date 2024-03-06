63727778ab78cd90e9328b19b688b72515d73558
J <133668286+alswla@users.noreply.github.com>
2024-03-06 16:02:38 +0900

    Setting: boilerplate

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/* eslint-disable global-require */
+/** @type {import('tailwindcss').Config} */
+
+const px50 = { ...Array.from(Array(51)).map((_, i) => `${i}px`) };
+const px600 = { ...Array.from(Array(601)).map((_, i) => `${i}px`) };
+const px800 = { ...Array.from(Array(801)).map((_, i) => `${i}px`) };
+
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      fontSize: px50,
+      width: px800,
+      height: px800,
+      spacing: px600,
+      zIndex: {
+        "-1": "-1",
+      },
+    },
+  },
+};
