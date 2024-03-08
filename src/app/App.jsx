d37fde49ecc1840150d2b6c1d15aa07177e15842
J <133668286+alswla@users.noreply.github.com>
2024-03-09 02:54:33 +0900

    Feat: add route

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,3 +1,16 @@
-function App() {}
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+
+import LoginPage from "../components/Login/LoginPage";
+
+function App() {
+  return (
+    <main className="box-border">
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+      </Routes>
+    </main>
+  );
+}
 
 export default App;
