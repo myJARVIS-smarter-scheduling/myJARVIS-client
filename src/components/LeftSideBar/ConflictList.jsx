15cfe51591d56ff8bf62496424adad794cf7bbc3
J <133668286+alswla@users.noreply.github.com>
2024-03-19 22:52:03 +0900

    Feat: create conflict list component

---
+++ src/components/LeftSideBar/ConflictList.jsx
@@ -0,0 +1,106 @@
+import { useEffect } from "react";
+import {
+  useLoginProviderStore,
+  useAccountEventStore,
+  useBiWeeklyEventListStore,
+} from "../../store/account";
+import ConflictSchedule from "./ConflictSchedule";
+
+function findConflicts(accounts) {
+  const conflicts = [];
+
+  accounts.forEach((account) => {
+    const { events } = account;
+    for (let i = 0; i < events.length; i += 1) {
+      for (let j = i + 1; j < events.length; j += 1) {
+        const eventA = events[i];
+        const eventB = events[j];
+
+        if (
+          new Date(eventA.startAt) < new Date(eventB.endAt) &&
+          new Date(eventB.startAt) < new Date(eventA.endAt)
+        ) {
+          conflicts.push({
+            account: account.accountId,
+            events: [eventA, eventB],
+          });
+        }
+      }
+    }
+  });
+
+  return conflicts;
+}
+
+function ConflictList() {
+  const { accounts, addConflict } = useAccountEventStore();
+  const { accountInfo } = useLoginProviderStore();
+  const { addBiWeeklyEvents } = useBiWeeklyEventListStore();
+  const now = new Date();
+  now.setHours(0, 0, 0, 0);
+
+  const firstDayOfWeek = new Date(now.setDate(now.getDate() - now.getDay()));
+  const lastDayOfWeek = new Date(now.setDate(firstDayOfWeek.getDate() + 6));
+  const lastDayOfBiweekly = new Date(
+    now.setDate(firstDayOfWeek.getDate() + 13),
+  );
+
+  useEffect(() => {
+    const conflictList = findConflicts(accounts);
+
+    const biWeeklyEvents = accounts.flatMap((account) =>
+      account.events.filter((event) => {
+        const start = new Date(event.startAt);
+        const end = new Date(event.endAt);
+        return start < lastDayOfBiweekly && end >= firstDayOfWeek;
+      }),
+    );
+
+    addConflict(conflictList);
+    addBiWeeklyEvents(biWeeklyEvents);
+  }, [accounts, addConflict, addBiWeeklyEvents]);
+
+  const weeklyEvents = accounts.flatMap((account) =>
+    account.events.filter((event) => {
+      const start = new Date(event.startAt);
+      const end = new Date(event.endAt);
+
+      return start < lastDayOfWeek && end > firstDayOfWeek;
+    }),
+  );
+
+  const Weeklyconflicts = [];
+
+  for (let i = 0; i < weeklyEvents.length; i += 1) {
+    for (let j = i + 1; j < weeklyEvents.length; j += 1) {
+      if (
+        new Date(weeklyEvents[i].startAt) < new Date(weeklyEvents[j].endAt) &&
+        new Date(weeklyEvents[j].startAt) < new Date(weeklyEvents[i].endAt)
+      ) {
+        Weeklyconflicts.push(weeklyEvents[i], weeklyEvents[j]);
+      }
+    }
+  }
+
+  console.log("Weekly conflictEvents", Weeklyconflicts);
+
+  return (
+    <main className="flex flex-col items-center justify-start w-full overflow-y-scroll">
+      {Weeklyconflicts.map((conflictPair) => {
+        const accountIndex = accountInfo.findIndex(
+          (account) => account.accountId === conflictPair.accountId,
+        );
+
+        return (
+          <ConflictSchedule
+            key={conflictPair.accounId}
+            conflictEvents={conflictPair}
+            accountIndex={accountIndex}
+          />
+        );
+      })}
+    </main>
+  );
+}
+
+export default ConflictList;
