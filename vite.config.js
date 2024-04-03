e6a66e9d30aa8f28864d8e41e85f8903eef36101
J <133668286+alswla@users.noreply.github.com>
2024-04-03 17:30:59 +0900

    Chore: add packages and setting conditions

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,15 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    css: true,
+    coverage: {
+      provider: "v8",
+      reporter: ["text"],
+    },
+    setupFiles: "./src/setupTests.js",
+    testMatch: ["./src/spec/*.spec.jsx"],
+  },
 });
