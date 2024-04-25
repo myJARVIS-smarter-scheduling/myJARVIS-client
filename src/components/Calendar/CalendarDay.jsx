c677b30ab92a6e994209d1a3b1ae503aa9db7db6
J <133668286+alswla@users.noreply.github.com>
2024-04-25 19:18:22 +0900

    Refactor: extract day rendering to a seperate module

---
+++ src/components/Calendar/CalendarDay.jsx
@@ -0,0 +1,18 @@
+/* eslint-disable */
+function CalendarDay({ dayLabels }) {
+  return (
+    <div className="flex">
+      {dayLabels.map((day, index) => (
+        <div
+          key={`${day}-${index}`}
+          style={{ width: "14.2857%" }}
+          className="flex-grow p-2 overflow-hidden text-center border-b-1 text-15 max-w-250:text-20"
+        >
+          {day}
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default CalendarDay;
